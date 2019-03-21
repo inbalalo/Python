6829643868a770d80bfafc917080eda209a2a80a
inbal alon <inbala@mail.tau.ac.il>
2019-03-21 14:05:02 +0200

    fixed syntax error

--- hw1_question1.py
+++ hw1_question1.py
@@ -5,7 +5,7 @@ def trifeca(word):
     returns: bool
     """
     for i in range(0, len(word)):
-        if (len(word)-i) => 6:
+        if (len(word)-i) >= 6:
             if word[i] == word[i+1] and word[i+2] == word[i+3] and word[i+4] == word[i+5]:
                return True
         else:
